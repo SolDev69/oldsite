f7f5340de79933f0c24956d845554aa771ff81f7
SolDev69 <40839581+SolDev69@users.noreply.github.com>
2024-01-23 21:56:54 +0000

    maybe this will work better? hello discord!

--- src/routes/downloads/+page.svelte
+++ src/routes/downloads/+page.svelte
@@ -6,9 +6,9 @@
 
 <h1>NeoKru Minecraft Mods</h1>
 <ul>
-    <li><a href="/neokru/downloads/expandedworlds">World Expansion (64bit and BigInteger Worldgen)</a></li>
-    <li><a href="/neokru/downloads/zetaforged">ZetaForged (v1/v2)</a></li>
-    <li><a href="/neokru/downloads/fringelands_mod">Allam A's Fringelands</a></li>
+    <li><a href="/downloads/expandedworlds">World Expansion (64bit and BigInteger Worldgen)</a></li>
+    <li><a href="/downloads/zetaforged">ZetaForged (v1/v2)</a></li>
+    <li><a href="/downloads/fringelands_mod">Allam A's Fringelands</a></li>
 </ul>
 <p>
     For any support, contact the NeoKru team at <a href="https://discord.gg/EDw2JmtQ9C">our Discord server</a>
